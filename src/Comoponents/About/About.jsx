d6a9fafc56bc6eae41d5605b9c50fce1f2198137
ErSakshiBhor <sakshibhor2005@gmail.com>
2025-04-26 05:14:26 -0700

    first commit

---
+++ src/Comoponents/About/About.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile from '../../assets/profile1.jpeg'
+
+const About = () => {
+  return (
+    <div id='about' className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+
+        <div className="about-sections">
+            <div className="about-left">
+                <img src={profile} alt="" />
+            </div>
+
+            <div className="about-right">
+                <div className="about-para">
+                    <p>I am a dedicated and enthusiastic Computer Engineering student with a strong interest in web development and programming.</p>
+                    <p>I enjoy building creative and user-friendly websites using technologies like HTML, CSS, JavaScript, ReactJS, and Python. I'm always eager to learn new skills, take on challenges, and grow as a developer.</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill">
+                        <p> HTML & CSS</p> <hr style={{width:"80%"}}/>
+                    </div>
+
+                    <div className="about-skill">
+                        <p> React JS</p> <hr style={{width:"70%"}}/>
+                    </div>
+
+                    <div className="about-skill">
+                        <p> Javascript</p> <hr style={{width:"70%"}}/>
+                    </div>
+
+                    <div className="about-skill">
+                        <p> Python</p> <hr style={{width:"60%"}}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ src/Comoponents/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Hero.css'
+import profile from '../../assets/profile.jpeg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile} alt="" />
+      <h1><span>Hii, I'm Sakshi Bhor,</span>  Python Full Stack Developer</h1>
+      <p>I am Sakshi Bhor, passionate about Full Stack Development using Python(Flask,Django)</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">
+            <a 
+              href="../../SakshiBhor_FullStackDeveloper.pdf" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="anchor-link"
+            >
+             My Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
